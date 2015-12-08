ddb446bb4f7c7ba6352dc710a8105962978a1921
connectwithrampl <connectwithrampl@gmail.com>
2015-12-07 20:17:07 -0800

    adding a basic sign in page

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+// random string to obfuscate
+var webappContext = '992f0957e1dcafc773d94f8ea4c9ba32ae9f4afe';
+
+function prefixRoute(path) {
+    var fSlash = '/';
+    if(path[0] === fSlash) {
+        return fSlash + webappContext  + path;
+    } else {
+        return fSlash + webappContext  + fSlash + path;
+    }
+}
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,12 +25,18 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/hello', function(request, response) {
-  response.end("<h2>Hello World</h2>");
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// routes prefixed with <code>webappContext</code>
+app.get(prefixRoute('sigin.html'), function(request, response) {
+    response.render('pages/signin');
+});
 
+app.get(prefixRoute('home.html'), function(request, response) {
+    var email = request.query.email;
+    console.log("got email: " + email);
+    // TODO: Validate
+    response.render('pages/home' , { email: email });
+});
