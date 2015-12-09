8d722624f656194f473e3b008fbf5e044b15fbd2
connectwithrampl <connectwithrampl@gmail.com>
2015-12-09 04:00:39 -0800

    refactoring themes and colors, adding sign out link

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var app = express();
+var winston = require('winston');
+var cookieParser = require('cookie-parser');
 
 // random string to obfuscate
 var webappContext = '992f0957e1dcafc773d94f8ea4c9ba32ae9f4afe';
@@ -13,8 +14,15 @@ function prefixRoute(path) {
     }
 }
 
-app.set('port', (process.env.PORT || 5000));
+function isCookiePresent(cookies) {
+  return cookies.email && cookies.email.trim() !== '';
+}
 
+winston.level = 'debug';
+
+var app = express();
+app.use(cookieParser());
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -26,17 +34,34 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  winston.info('conceptbase app started on port', app.get('port'));
 });
 
 // routes prefixed with <code>webappContext</code>
 app.get(prefixRoute('signin.html'), function(request, response) {
-    response.render('pages/signin');
+    var cookies = request.cookies;
+    if (isCookiePresent(cookies)) {
+      response.redirect(prefixRoute('home.html'));
+    } else {
+      response.render('pages/signin');
+    }
 });
 
+// this sets the 'email' cookie based on if 'email' query param was present or not
 app.get(prefixRoute('home.html'), function(request, response) {
+    var cookies = request.cookies;
     var email = request.query.email;
-    console.log("got email: " + email);
-    // TODO: Validate
-    response.render('pages/home' , { email: email });
+    if (email && email !== '') {
+      response.cookie('email', email, { maxAge: 900000, httpOnly: false });
+      winston.debug("set cookie to %s", email);
+    } else {
+      if(isCookiePresent(cookies)) {
+        winston.debug("cookie already set to: %s", cookies.email);
+      } else {
+        response.status(400).send("<b>You have been signed out</b>");
+        response.end();
+        return;
+      }
+    }
+    response.render('pages/home');
 });
