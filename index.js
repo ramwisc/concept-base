c6d23c4616a70658ceb9e35415a45e3153f4cbf1
connectwithrampl <connectwithrampl@gmail.com>
2015-12-11 05:07:23 -0800

    adding data layer for resources and moving to node 4.3.2

--- index.js
+++ index.js
@@ -68,26 +68,22 @@ app.listen(app.get('port'), function() {
 
 app.all(prefixRoute('/*'), requireAuthentication);
 
-app.get(prefixRoute('/'), function(request, response) {
-  response.render('pages/home');
-});
-
 app.get(prefixRoute('/home.html'), function(request, response) {
   response.render('pages/home');
 });
 
 //--------REST data endpoints-------------------------------------------------//
-/**
- * Create a new concept in the datastore
- */
-app.post(prefixRoute("/concept"), function(req, res) {
+
+//CRUD on concepts
+app.post(prefixRoute("/domain/:domain/concept"), function(req, res) {
   var concept = req.body;
   if(!concept) {
     var error = { msg: 'concept object was not present in the request or marlformed!' };
     res.status(400).send(JSON.stringify(error));
   } else {
     winston.debug("received concept %s", JSON.stringify(concept));
-    conceptStore.add(concept, req.user, function(conceptId, error) {
+    var domain = req.params.domain;
+    conceptStore.addConcept(domain, concept, req.user, function(conceptId, error) {
       if(error) {
         var msg = { msg: error };
         res.status(500)
@@ -103,11 +99,12 @@ app.post(prefixRoute("/concept"), function(req, res) {
   }
 });
 
-app.get(prefixRoute("/concept/:id"), function(req, res) {
-  winston.debug("retrieving concept %s", req.params.id);
-  conceptStore.get(req.params.id, function(concept) {
+app.get(prefixRoute("/domain/:domain/concept/:id"), function(req, res) {
+  winston.debug("retrieving concept %s in domain %s", req.params.id, req.params.domain);
+  conceptStore.getConcept(req.params.domain, req.params.id, function(concept) {
     if(!concept) {
-      var error = util.format("concept '%s' not found!", req.params.id);
+      var error = util.format("concept '%s' not found in domain %s!",
+                                      req.params.id, req.params.domain);
       res.status(404)
         .set('Content-Type', 'application/json')
         .send(JSON.stringify({msg : error}));
@@ -116,5 +113,90 @@ app.get(prefixRoute("/concept/:id"), function(req, res) {
           .set('Content-Type', 'application/json')
           .send(JSON.stringify(concept));
     }
+  });
+});
+
+// CRUD on resources
+app.post(prefixRoute("/domain/:domain/concept/:concept/resource"), function(req, res) {
+  var resource = req.body;
+  if(!resource) {
+    var error = { msg: 'resource object was not present in the request or marlformed!' };
+    res.status(400).send(JSON.stringify(error));
+  } else {
+    winston.debug("received resource %s", JSON.stringify(resource));
+    var domain = req.params.domain;
+    var concept = req.params.concept;
+    conceptStore.addResource(domain, concept, resource, req.user, function(resourceId, error) {
+      if(error) {
+        var msg = { msg: error };
+        res.status(500)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(msg));
+      } else {
+        var msg = { id: resourceId };
+        res.status(201)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(msg));
+      }
+    });
+  }
+});
+
+app.get(prefixRoute("/domain/:domain/concept/:concept/resource/:id"), function(req, res) {
+  winston.debug("retrieving resource %s in concept %s and domain %s",
+                            req.params.id, req.params.concept, req.params.domain);
+  conceptStore.getResource(req.params.domain, req.params.concept, req.params.id,
+    function(resource) {
+      if(!resource) {
+        var error = util.format("resource '%s' not found in concpet %s and domain %s!",
+                                        req.params.id, req.params.concept, req.params.domain);
+        res.status(404)
+          .set('Content-Type', 'application/json')
+          .send(JSON.stringify({msg : error}));
+      } else {
+        res.status(200)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(resource));
+      }
+  });
+});
+
+app.post(prefixRoute("/domain/:domain/concept/:concept/resource/:id/like"), function(req, res) {
+  var domain = req.params.domain;
+  var concept = req.params.concept;
+  var resourceId = req.params.id;
+  winston.debug("+1 for resource %s in concept %s, domain %s, user %s",
+      resourceId, concept, domain, req.user);
+  conceptStore.likeResource(domain, concept, resourceId, req.user,
+    function(likeCount, error) {
+      if(error) {
+        res.status(500)
+          .set('Content-Type', 'application/json')
+          .send(JSON.stringify({ msg : error }));
+      } else {
+        res.status(200)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify({ likes: likeCount }));
+      }
+  });
+});
+
+app.post(prefixRoute("/domain/:domain/concept/:concept/resource/:id/unlike"), function(req, res) {
+  var domain = req.params.domain;
+  var concept = req.params.concept;
+  var resourceId = req.params.id;
+  winston.debug("-1 for resource %s in concept %s, domain %s, user %s",
+      resourceId, concept, domain, req.user);
+  conceptStore.unlikeResource(domain, concept, resourceId, req.user,
+    function(likeCount, error) {
+      if(error) {
+        res.status(500)
+          .set('Content-Type', 'application/json')
+          .send(JSON.stringify({ msg : error }));
+      } else {
+        res.status(200)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify({ likes: likeCount }));
+      }
   });
 });
--- store/concept-store.js
+++ store/concept-store.js
@@ -1,55 +1,210 @@
 /**
- * Module that provides data access to the concepts
+ * Module that provides data access to the concepts within domains
  */
-
 var winston = require('winston');
 var util = require('util');
+var uuid = require('uuid');
 
+/**
+ * Represents an InMemoryStore of concepts in various domainToConcepts
+ * @constructor
+ */
 function InMemoryStore() {
-  // map from concept id -> concept object
-  this.concepts = {};
+  // { domain -> conceptId -> concept}
+  this.domainToConcepts = {};
+  // note that resourceIds are UUIDs.
+  // { resourceId -> resource }
+  this.resources = {};
 }
 
-// aka quora style for id and we will this as the primary key
-function _generateConceptId(conceptName) {
+/**
+ * aka quora style for generating concepts ids
+ * @private
+ */
+function generateConceptId(conceptName) {
   return conceptName.split(/\s+/).join("-");
 }
 
-InMemoryStore.PROTOTYPE_CONCEPT = {
+InMemoryStore.PROTOTYPE_CONCEPT =  JSON.stringify({
     "id" : null,
     "name" : null,
+    "domain": null,
     "description": "",
-    "resouces": [],
+    "resources": [], // array of resource_ids
+    // dummy values, will be replaced
     "created_at" : "2015-12-10T20:40:49.318Z",
     "created_by": "test@gmail.com"
-};
+});
 
-InMemoryStore.prototype.add = function(concept, user, callback) {
+/**
+ * @param {string} domain
+ * @param {string} concept
+ * @param {string} user use who is adding the concept in the given domain
+ * @param {Function} callback with the (conceptId, null) else with (null, error)
+ * @public
+ */
+InMemoryStore.prototype.addConcept = function(domain, concept, user, callback) {
   if (!concept.name) {
     callback(null,
       util.format("key 'name' is missing in %s", JSON.stringify(concept)));
   } else {
-    var storedConcept = Object.create(InMemoryStore.PROTOTYPE_CONCEPT);
-    storedConcept.id = _generateConceptId(concept.name);
+    var storedConcept = JSON.parse(InMemoryStore.PROTOTYPE_CONCEPT);
+    winston.debug("prototyped %s", JSON.stringify(storedConcept));
+    storedConcept.id = generateConceptId(concept.name.toLowerCase());
     storedConcept.name = concept.name;
+    storedConcept.domain = domain;
     storedConcept.description = concept.description || "";
     storedConcept.created_at = new Date().toISOString();
     storedConcept.created_by = user;
     winston.debug("adding concept %s", JSON.stringify(storedConcept));
-    this.concepts[storedConcept.id] = storedConcept;
+    // populate the domain and concept hierarchy
+    this.domainToConcepts[domain] = this.domainToConcepts[domain] || {};
+    var concepts = this.domainToConcepts[domain];
+    concepts[storedConcept.id] = storedConcept;
     callback(storedConcept.id, null); // success adding a concept!
   }
 }
 
-InMemoryStore.prototype.get = function(id, callback) {
-  var concept = this.concepts[id];
-  if(!concept) {
-    winston.warn("concept with id '%s' not found", id);
-    callback(null); // signals we didn't find it.
-  } else {
-    callback(concept);
+/**
+ * @param {string} domain
+ * @param {string} id the concept id
+ * @param {Function} callback with the concept object if found else null
+ * @public
+ */
+
+InMemoryStore.prototype.getConcept = function(domain, id, callback) {
+  var concepts = this.domainToConcepts[domain];
+  if(!concepts) { callback(null); }
+  else {
+    var concept = concepts[id];
+    if(!concept) {
+      callback(null);
+    } else {
+      callback(concept);
+    }
   }
 }
+
+InMemoryStore.PROTOTYPE_RESOURCE =  JSON.stringify({
+    "id" : null,
+    "description": "null",
+    "link" : null,
+    "conceptId": null,
+    "likedBy" : {}, // map of users who like this resource
+    "numLikes" : 0, // pre-computed from likedBy
+    // dummy values, will be replaced
+    "created_at" : "2015-12-10T20:40:49.318Z",
+    "created_by": "test@gmail.com"
+});
+
+/**
+ * @param {string} concept
+ * @param {string} link link to a resource for the concept
+ * @param {string} user use who is adding the resource to the concept
+ * @param {Function} callback with the (resourceId, null) else with (null, error)
+ * @public
+ */
+InMemoryStore.prototype.addResource = function(domain, conceptId, resource, user, callback) {
+  if (!resource.link) {
+    callback(null,
+      util.format("key 'link' is missing in %s", JSON.stringify(resource)));
+  } else {
+    var storedResource = JSON.parse(InMemoryStore.PROTOTYPE_RESOURCE);
+    storedResource.id = uuid.v1();
+    storedResource.link = resource.link;
+    storedResource.description = resource.description;
+    storedResource.conceptId = conceptId;
+    storedResource.created_at = new Date().toISOString();
+    storedResource.created_by = user;
+    winston.debug("adding resource %s", JSON.stringify(storedResource));
+    var concepts = this.domainToConcepts[domain];
+    if(!concepts) {
+      callback(null, util.format('domain %s missing!', domain));
+    } else if(!concepts[conceptId]) {
+      callback(null, util.format('conceptId %s missing in domain %s!', conceptId, domain));
+    } else {
+      concepts[conceptId].resources.push(storedResource.id);
+      this.resources[storedResource.id] = storedResource;
+      callback(storedResource.id, null); // success adding a concept!
+    }
+  }
+}
+
+/**
+ * @param {string} domain
+ * @param {string} conceptId
+ * @param {string} resId the resource id
+ * @param {Function} callback with the resource object if found else null
+ * @public
+ */
+
+InMemoryStore.prototype.getResource = function(domain, conceptId, resId, callback) {
+  var concepts = this.domainToConcepts[domain];
+  if(!concepts || !concepts[conceptId]) {
+    callback(null);
+  } else {
+    var resource = this.resources[resId];
+    if(!resource) {
+      callback(null);
+    } else {
+      callback(resource);
+    }
+  }
+}
+
+/**
+ * @param {string} domain
+ * @param {string} conceptId
+ * @param {string} resId the resource id
+ * @param {string} user the user liking or unliking the resource
+ * @param {delta} +1 for like and -1 for unlike
+ * @param {Function} callback with the like count if resource found else error
+ * @private
+ */
+InMemoryStore.prototype.adjustResLikeCount = function(domain, conceptId, resId, user, delta, callback) {
+  if(delta !== 1 && delta !== -1) {
+    callback(util.format('delta %s is invalid. delta can only be +1 or -1.',
+                                                                String(delta)));
+  } else {
+    this.getResource(domain, conceptId, resId, function(resource) {
+      if(!resource) {
+        var error = util.format("resource %s not found", resId);
+        callback(null, error);
+      } else {
+        resource.likedBy[user] = (delta === 1) ? true : false;
+        resource.numLikes = resource.numLikes + delta;
+        callback(resource.numLikes, null);
+      }
+    });
+  }
+}
+
+/**
+ * @param {string} domain
+ * @param {string} conceptId
+ * @param {string} resId the resource id
+ * @param {string} user the user liking the resource
+ * @param {Function} callback with the like count if resource found else error
+ * @public
+ */
+
+InMemoryStore.prototype.likeResource = function(domain, conceptId, resId, user, callback) {
+  this.adjustResLikeCount(domain, conceptId, resId, user, 1, callback);
+}
+
+/**
+ * @param {string} domain
+ * @param {string} conceptId
+ * @param {string} resId the resource id
+ * @param {string} user the user unliking the resource
+ * @param {Function} callback with the like count if resource found else error
+ * @public
+ */
+
+InMemoryStore.prototype.unlikeResource = function(domain, conceptId, resId, user, callback) {
+  this.adjustResLikeCount(domain, conceptId, resId, user, -1, callback);
+}
+
 
 var exports = module.exports = {};
 exports.conceptStore = new InMemoryStore();
